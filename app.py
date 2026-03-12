4527ec4e0b66228e4e026d0da798b66dedc4b3f0
Neudes1910 <138127675+Neudes1910@users.noreply.github.com>
2026-03-12 11:58:23 -0300

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def set_background(png_file):
     ),
     url("data:image/png;base64,{bin_str}");
 
-    background-size: 500px auto;
+    background-size: 300px auto;
     background-position: top right;
     background-attachment: fixed;
     background-repeat: no-repeat;
@@ -260,6 +260,7 @@ if uploaded_files:
 else:
 
     st.info("Aguardando envio dos relatórios.")
+
 
 
 
